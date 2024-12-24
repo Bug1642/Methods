aa1411c8918626537b59719fd9062ba994e25f85
Bug1642 <hasancan1642@hotmail.com>
2024-12-24 15:31:30 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Exercise {
+    public class StudentCode {
+        public static string GreetMe(string name){
+            return "Hello,"+ name;
+        }
+    }
+    class Program{
+        static void Main(string[] args ){
+            Console.WriteLine(StudentCode.GreetMe("Adam"));
+        }
+    }
+}
