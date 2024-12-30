f3a70c5c7495bc905c5654c6780ee737bcf2dfb6
Bug1642 <hasancan1642@hotmail.com>
2024-12-30 14:49:31 +0300

    First Commit

--- obj/Release/net9.0/Methods.AssemblyInfo.cs
+++ obj/Release/net9.0/Methods.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Methods")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa1411c8918626537b59719fd9062ba994e25f85")]
 [assembly: System.Reflection.AssemblyProductAttribute("Methods")]
